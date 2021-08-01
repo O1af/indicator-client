de0222150ba21484642c9548832ac2d22636adbf
Olaf Dsouza <olafdsouza27@gmail.com>
2021-08-01 17:09:25 -0400

    updated title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{'Job Indicator'}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={'Job Indicator'} />
       </Helmet>
